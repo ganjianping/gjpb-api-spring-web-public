bc2a7c220063fe63467f1f4e3e7e879d3ae93bac
ganjianping <ganjianping1984@gmail.com>
2025-05-12 23:07:12 +0800

    Update application with fixed test for default dev profile and add new files to project structure

---
+++ src/test/java/org/ganjp/blog/RootControllerTest.java
@@ -0,0 +1,34 @@
+package org.ganjp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RootController.class)
+class RootControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRootEndpoint() throws Exception {
+        // Test that the root endpoint returns the welcome message
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Welcome to Gan Jian Ping Blog APIs")));
+    }
+    
+    @Test
+    void testRootEndpointContentType() throws Exception {
+        // Test that the response content type is text/plain
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"));
+    }
+}
