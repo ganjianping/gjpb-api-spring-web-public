8e6d8fcf8a3ced9ff7421d640a266327ddde30c7
ganjianping <ganjianping1984@gmail.com>
2025-11-07 23:12:12 +0800

    Commit all workspace changes

---
+++ devops/ansible/create_folders.sh
@@ -0,0 +1,4 @@
+# chmod 700 login.sh
+# chmod 400 ~/.ssh/id_rsa
+
+ansible-playbook ./playbook/create_folders.yml -i ~/.ansible/inventory/hosts -l ubuntu_server
--- devops/ansible/install_jdk.sh
+++ devops/ansible/install_jdk.sh
@@ -1,4 +1,4 @@
 # chmod 700 login.sh
 # chmod 400 ~/.ssh/id_rsa
 
-ansible-playbook ./playbook/install_jdk.yml -i ~/.ansible/inventory/hosts -l ubuntu_server
+ansible-playbook ./playbook/create_upload_folders.yml -i ~/.ansible/inventory/hosts -l ubuntu_server
